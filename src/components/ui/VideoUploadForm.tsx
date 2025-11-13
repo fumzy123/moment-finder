651313a50425436a2260fb960ac38d61004b0397
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2025-11-12 22:47:34 -0330

    created a client component to upload videos

---
+++ src/components/ui/VideoUploadForm.tsx
@@ -0,0 +1,40 @@
+import { useState, type FormEvent } from "react";
+
+export default function VideoUploadForm() {
+    // State
+    const [responseMessage, setResponseMessage] = useState("");
+
+    // Handle Event
+    async function handleVideoUpload(event: FormEvent<HTMLFormElement>) {
+        // Prevent the default form submission behavior of the browser
+        event.preventDefault();
+
+        const formData = new FormData(event.target as HTMLFormElement);
+        const response = await fetch('/api/videos/', {
+            method: 'POST',
+            body: formData
+        })
+        const data = await response.json();
+
+        if (data.message) {
+            setResponseMessage(data.message);
+        }
+    }
+
+    // Render
+    return (
+        <form method="POST" onSubmit={handleVideoUpload}>
+            <div>
+                <label htmlFor="selectedVideo">Upload a Video</label>
+                <input
+                    type="file"
+                    id="selectedVideo"
+                    name="selectedVideo"
+                    accept="video/*"
+                />
+            </div>
+            <button type="submit">Submit</button>
+            {responseMessage && <p>{responseMessage}</p>}
+        </form>
+    )
+}
