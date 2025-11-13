fcc37bed701d2e1c1ae5281073dfe6d1d122027a
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2025-11-13 01:31:23 -0330

    updated the name of the application function to better reflect its purpose

--- src/features/videos/application/getUploadedVideos.ts
+++ src/features/videos/application/getUploadedVideos.ts
@@ -4,11 +4,11 @@ import type { Video } from "../entities/Video";
 // Ports / Infrastructure
 import type { IVideoRepo } from "../ports/IVideoRepo"
 
-interface ListVideosInfrastructure {
+interface GetUploadedVideosInfrastructure {
     videoRepo: IVideoRepo
 }
 
-export async function listVideos ({ infrastructure }: {infrastructure: ListVideosInfrastructure}): Promise<Video[]> {
+export async function getUploadedVideos ({ infrastructure }: {infrastructure: GetUploadedVideosInfrastructure}): Promise<Video[]> {
     const { videoRepo } = infrastructure;
     const videos = await videoRepo.getVidoes();
     return Promise.all(
