33ad193715277d6a447c9d93af8c8e4f1e6ae26d
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2025-11-13 00:21:16 -0330

    update port and infra for video

--- src/features/videos/infrastructure/GoogleCloudStorageVideoRepo.ts
+++ src/features/videos/infrastructure/GoogleCloudStorageVideoRepo.ts
@@ -1,21 +1,21 @@
+import type { Video } from "../entities/Video";
 import type { IVideoRepo } from "../ports/IVideoRepo";
-import { Storage } from '@google-cloud/storage';
+import { Bucket, Storage, type GetFilesResponse } from '@google-cloud/storage';
 
 export class GoogleCloudStorageVideoRepo implements IVideoRepo {
     private storage: Storage;
-    private uploadedVideosBucket: any;
+    private uploadedVideosBucket: Bucket;
 
     constructor(storage: Storage, bucketName = "video-moments-upload") {
         this.storage = storage;
         this.uploadedVideosBucket = this.storage.bucket(bucketName);
     }
 
-    async getVidoes(): Promise<File[]> {
-        const [files] = await this.uploadedVideosBucket.getFiles();
-        return files;
-    }
-
     async generateSignedUrlforVideoFile(fileName: string): Promise<string> {
+        if (!fileName) {
+            console.warn("⚠️ No file name provided to generateSignedUrlforVideoFile");
+            return "";
+        }
         const gcsFile = this.uploadedVideosBucket.file(fileName);
         const [url] = await gcsFile.getSignedUrl({
             action: 'read',
@@ -24,7 +24,22 @@ export class GoogleCloudStorageVideoRepo implements IVideoRepo {
         return url;
     }
 
-    async uploadVideo(selectedVideo: File): Promise<{url: string, success: boolean, message: string}> {
+    async getVidoes(): Promise<Video[]> {
+        // Get list of files from GCS bucket
+        const [files] = await this.uploadedVideosBucket.getFiles();
+
+        // Map files to Video entities with signed URLs
+        const videos = await Promise.all(
+            files.map(async (file) => ({
+                name: file.name,
+                url: await this.generateSignedUrlforVideoFile(file.name),
+            }))
+        );
+        return videos;
+    }
+
+
+    async uploadVideo(selectedVideo: File): Promise<Video> {
 
         console.log("Uploading file to GCS:", selectedVideo.name);
 
@@ -32,23 +47,19 @@ export class GoogleCloudStorageVideoRepo implements IVideoRepo {
         const arrayBuffer = await selectedVideo.arrayBuffer();
         const buffer = Buffer.from(arrayBuffer);
 
-        // Create a file object in the bucket and then upload the binary data
+        // Create a file object in the bucket and then upload the binary data to that file object
         const gcsFile = this.uploadedVideosBucket.file(selectedVideo.name);
         await gcsFile.save(buffer, {
             contentType: selectedVideo.type
         })
 
-        // Get a signed URL for accessing the uploaded file
-        const [url] = await gcsFile.getSignedUrl({
-            action: 'read',
-            expires: Date.now() + 1000 * 60 * 60 // 1 hour
-        })
-        console.log(`File uploaded to GCS: ${url}`);
-        return {
-                success: true,
-                message: `Uploaded ${selectedVideo.name} successfully`,
-                url,
-        };
+        // Convert uploaded file to Video entity
+        const video = {
+            name: gcsFile.name,
+            url: await this.generateSignedUrlforVideoFile(gcsFile.name),
+        }
+
+        return video;
     }
 
     async deleteVideo(): Promise<void> {
--- src/features/videos/ports/IVideoRepo.ts
+++ src/features/videos/ports/IVideoRepo.ts
@@ -1,7 +1,8 @@
+import type { Video } from "../entities/Video";
 
 
 export abstract class IVideoRepo {
-    abstract getVidoes(): Promise<File[]>;
     abstract generateSignedUrlforVideoFile(fileName: string): Promise<string>;
-    abstract uploadVideo(file: File): Promise<{url: string, success: boolean, message: string}>;
+    abstract getVidoes(): Promise<Video[]>;
+    abstract uploadVideo(file: File): Promise<Video>;
 }
