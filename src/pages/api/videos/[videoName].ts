fa6b999c189d2b16be0853f382c55b6e4b9be2cf
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2025-11-13 20:32:28 -0330

    setup api endpoint to retrieve video based on name and apply it to the page

---
+++ src/pages/api/videos/[videoName].ts
@@ -0,0 +1,27 @@
+import type { APIRoute } from "astro";
+
+// Dependency Injection Resolvers
+import { resolvers } from "../../../dependency-injection/resolvers";
+
+// Application layer
+import { getVideo } from "../../../features/videos/application/getVideo";
+
+export const GET : APIRoute = async ({ params } ) => {
+    console.log('API params:', params);
+    const { videoName } = params;
+
+    // Get the Dependecnies from the Dependency Injection Container
+    const videoRepo = resolvers.videoRepoResolver();
+    
+    // Call the Application layer to get the video
+    const video = await getVideo({
+        infrastructure: {
+            videoRepo
+        },
+        args: {
+            videoName: videoName!,
+        }
+    });
+    console.log("Found Video", video)
+    return new Response(JSON.stringify({ data: video, error: null, message: `Video successfully fetched ${videoName}` }));
+}
