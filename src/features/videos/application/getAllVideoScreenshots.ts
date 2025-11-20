85864ff74e174ce00912907faeb53e3505515b1d
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2025-11-20 18:41:49 -0330

    removed unused import

--- src/features/videos/application/getAllVideoScreenshots.ts
+++ src/features/videos/application/getAllVideoScreenshots.ts
@@ -1,28 +1,34 @@
 import type { IVideoScreenshotMetadataRepo } from "../../videoScreenshotsMetadata/ports/IVideoScreenshotMetadataRepo";
-import type { Video, VideoScreenshot } from "../entities/Video";
+import type { Video } from "../entities/Video";
 import type { IVideoRepo } from "../ports/IVideoRepo";
 
 export interface GetVideoScreenshotInfrastructure {
-    videoRepo: IVideoRepo,
-    videoScreenshotMetadataRepo: IVideoScreenshotMetadataRepo,
+  videoRepo: IVideoRepo;
+  videoScreenshotMetadataRepo: IVideoScreenshotMetadataRepo;
 }
 
 export interface GetVideoScreenshotArgs {
-    videoId: Video["id"],
-    
+  videoId: Video["id"];
 }
 
+export async function getAllVideoScreenshots({
+  infrastructure,
+  args,
+}: {
+  infrastructure: GetVideoScreenshotInfrastructure;
+  args: GetVideoScreenshotArgs;
+}) {
+  const { videoRepo, videoScreenshotMetadataRepo } = infrastructure;
+  const { videoId } = args;
 
-export async function getAllVideoScreenshots({infrastructure, args}: { infrastructure: GetVideoScreenshotInfrastructure, args: GetVideoScreenshotArgs }) {
-    const {videoRepo, videoScreenshotMetadataRepo} = infrastructure;
-    const { videoId } = args;
+  // Get all the Screenshots for this video
+  const videoScreenshot = await videoRepo.getVideoScreenshots(
+    videoId
+  );
+  // console.log(`Application Layer: GetAllVideoScreenshot with ${videoId}`, videoScreenshot)
 
-    // Get all the Screenshots for this video
-    const videoScreenshot = await videoRepo.getVideoScreenshots(videoId);
-    // console.log(`Application Layer: GetAllVideoScreenshot with ${videoId}`, videoScreenshot)
+  // Get all the Metadata for the screenshots
+  // const videoScreenshotMetadata = await videoScreenshotMetadataRepo.getAllVideoScreenshotMetadata(videoId)
 
-    // Get all the Metadata for the screenshots
-    // const videoScreenshotMetadata = await videoScreenshotMetadataRepo.getAllVideoScreenshotMetadata(videoId)
-
-    return videoScreenshot
+  return videoScreenshot;
 }
