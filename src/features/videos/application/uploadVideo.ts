a7eb9eebef211fb8f736bf2fd273f0d891eac277
Fume Uvwie <25962091+fumzy123@users.noreply.github.com>
2025-11-20 18:04:04 -0330

    updating the response type of the application function

--- src/features/videos/application/uploadVideo.ts
+++ src/features/videos/application/uploadVideo.ts
@@ -1,40 +1,49 @@
-
 // Ports
-import type { IVideoRepo } from "../ports/IVideoRepo"
-
+import type { IVideoRepo } from "../ports/IVideoRepo";
 
 // Interface types
 interface UploadVideoInfrastructure {
-    videoRepo: IVideoRepo
+  videoRepo: IVideoRepo;
 }
 
 interface UploadVideoArgs {
-    // Add any parameters needed for uploading videos, e.g., file data
-    selectedVideo: File;
+  // Add any parameters needed for uploading videos, e.g., file data
+  selectedVideo: File;
 }
 
 interface UploadVideoResponse {
-    uploadedVideoUrl: string;
-    success: boolean;
-    message: string;
+  id: string;
+  name: string;
+  bucketPath: string;
+  displayName?: string | undefined;
+  uploadedVideoUrl: string;
 }
 
-export async function uploadVideo ({ infrastructure, args }: {infrastructure: UploadVideoInfrastructure, args: UploadVideoArgs}): Promise<UploadVideoResponse> {
+export async function uploadVideo({
+  infrastructure,
+  args,
+}: {
+  infrastructure: UploadVideoInfrastructure;
+  args: UploadVideoArgs;
+}): Promise<UploadVideoResponse> {
+  // Extract Infrastructure and Arguments
+  const { videoRepo } = infrastructure;
+  const { selectedVideo } = args;
 
-    // Extract Infrastructure and Arguments
-    const { videoRepo } = infrastructure;
-    const { selectedVideo } = args;
+  // Upload the video using the Video Adapter
+  const uploadedVideoFile = await videoRepo.uploadVideo(
+    selectedVideo
+  );
 
-    // Upload the video using the Video Adapter
-    const uploadedVideoFile = await videoRepo.uploadVideo(selectedVideo);
-
-    // Return the upload result
-    // Get a signed URL for accessing the uploaded file
-    const signedVideoUrl = await videoRepo.generateSignedUrlforMediaFile(uploadedVideoFile.name);
-    console.log(`File uploaded to GCS: ${signedVideoUrl}`);
-    return {
-        success: true,
-        message: `Uploaded ${uploadedVideoFile.name} successfully`,
-        uploadedVideoUrl: signedVideoUrl,
-    };
+  // Return the upload result
+  // Get a signed URL for accessing the uploaded file
+  const signedVideoUrl =
+    await videoRepo.generateSignedUrlforMediaFile(
+      uploadedVideoFile.name
+    );
+  console.log(`File uploaded to GCS: ${signedVideoUrl}`);
+  return {
+    ...uploadedVideoFile,
+    uploadedVideoUrl: signedVideoUrl,
+  };
 }
